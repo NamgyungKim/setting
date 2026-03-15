17c959a195286b4afffee9f47e83e3d3481c277b
NamgyungKim <namgyung.kim@gmail.com>
2026-03-15 22:13:24 +0900

    fix

--- github/index.sh
+++ github/index.sh
@@ -4,5 +4,5 @@ if [ "${REMOTE:-false}" = true ]; then
 	bash <(curl -fsSL "$BASE_URL/github/alias/index.sh")
 else
 	SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-	sh "$SCRIPT_DIR/alias/index.sh"
+	bash "$SCRIPT_DIR/alias/index.sh"
 fi
